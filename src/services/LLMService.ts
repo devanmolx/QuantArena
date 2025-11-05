b44d1685437f8558ee2cdf34a5b39f29f16ca7c1
Anmol Garg <anmolgarg271005@gmail.com>
2025-11-05 16:19:33 +0530

    use qwen llm model

--- src/services/LLMService.ts
+++ src/services/LLMService.ts
@@ -1,9 +1,11 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatOpenAI } from "@langchain/openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const llm = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-flash",
-    apiKey: process.env.GEMINI_API_KEY || "",
-    temperature: 0.4,
+export const llm = new ChatOpenAI({
+    model: "qwen/qwen3-32b",
+    apiKey: process.env.GROQ_API_KEY || "",
+    configuration: {
+        baseURL: "https://api.groq.com/openai/v1"
+    }
 });
